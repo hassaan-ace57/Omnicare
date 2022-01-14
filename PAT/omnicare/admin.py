b0c61727956cbb00046e5a1706334736401f2725
hassaan-ace57 <84634502+hassaan-ace57@users.noreply.github.com>
2022-01-14 23:32:41 +0500

    Add files via upload

---
+++ PAT/omnicare/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Nurse)
+admin.site.register(Patient)
+admin.site.register(Medicine)
+admin.site.register(Bill)
+admin.site.register(Supplier)
+admin.site.register(UserProfile)
